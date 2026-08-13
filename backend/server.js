9bd663a960b6c52bd19d1eeaa4a7cc3ec1191d5b
marcos11aa11 <marcoseduardo0770@gmail.com>
2026-08-12 21:53:06 -0400

    primeiro servidor node

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
 
+app.use((req, res, next)=> {
+    console.log("Acesso:", req.method, req.url);
+    next();
+})
+
+app.get("/", (req, res) => {
+    res.send(`
+        <h1>Menu</h1>
+        <a href="/aluno/Milena">Ir para aluno</a><br>
+        <a href="/status">Ir para status</a>
+        `);
+});
+
 // 1. Define uma rota (endpoint)
 app.get('/', (req, res) => {
     res.send('Hello World! Este é o primeiro servidor backend funcionando');
@@ -20,6 +33,15 @@ app.get("/aluno/:a/:b", (req, res) => {
     const b = Number(req.params.b);
     const resultado = a + b;
     res.send(`O resultado é ${resultado}!`);
+})
+
+app.get("/status", (req, res) => {
+    res.json({
+        servidor: "online",
+        disciplina: "CP3",
+        professora: "Milena",
+        hora: new Date().toLocaleString()
+    })
 })
 
 // 2. Lida o servidor para escutar na porta 3000
